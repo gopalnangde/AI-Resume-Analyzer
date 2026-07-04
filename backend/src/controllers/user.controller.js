9509f3e42100299edf0ec1bfa93ebda4e2f30e20
gopalnangde <gopalnangde5@gmail.com>
2026-07-04 18:19:53 +0530

    sign-in and sig-out funtionality added

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,45 +1,127 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/user.model.js';
-const signup =async(req,res) =>{
-    const {name,email,password} = req.body;
+const signup = async (req, res) => {
+    const { name, email, password } = req.body;
     try {
-        if(!name || !email || !password){
-            return res.status(401).json({
-                message:"All fields are manditory"
-            })
-        }
-        const userExist = await userModel.findOne({});
-        if(userExist){
+        if (!name || !email || !password) {
             return res.status(400).json({
-                message:"User already registered"
+                message: "All fields are mandatory"
             })
         }
-        const hashPass = await bcrypt.hash(password,10);
+
+        const userExist = await userModel.findOne({ email });
+        if (userExist) {
+            return res.status(400).json({
+                message: "User already registered"
+            })
+        }
+
+        const hashPass = await bcrypt.hash(password, 10);
         const user = await userModel.create({
-            name,email,password:hashPass
+            name,
+            email,
+            password: hashPass
         })
-        
+
         const token = jwt.sign({
-            id:user._id
-        }, process.env.JWT_SECRET);
-        res.cookie("token",token);
-        res.send(200).json({
-            message:"User register successfully",
-            user:{
-                _id:user._id,
-                email:user.email,
-                name:user.name
+            id: user._id
+        }, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        });
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.status(201).json({
+            message: "User registered successfully",
+            user: {
+                _id: user._id,
+                email: user.email,
+                name: user.name
             }
         })
     } catch (error) {
         console.log(error);
+        return res.status(500).json({
+            message: "Signup failed"
+        })
     }
 
 }
 
 const login = async(req,res) =>{
-    
+    const {email,password} = req.body;
+    try {
+        if (!email || !password) {
+          return res.status(400).json({
+            message: "All field are mandatory",
+          });
+        }
+        const user = await userModel.findOne({ email });
+        if (!user) {
+          return res.status(401).json({
+            message: "Invalid credentials",
+          });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return res.status(401).json({
+            message: "Invalid credentials",
+          });
+        }
+
+        const token = jwt.sign(
+          {
+            id: user._id,
+          },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "7d",
+          },
+        );
+        //Store token in cookie
+        res.cookie("token", token, {
+          httpOnly: true,
+          sameSite: "strict",
+          maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+        res.status(200).json({
+          message: "User login successfully",
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+          },
+        });
+
+    } catch (error) {
+        console.log("Error: ", error);
+        return res.status(500).json({
+            message:"Internal server error"
+        });
+    }
+
 }
 
-export {signup,login}
+const logout = async(req,res)=>{
+    try {
+        res.clearCookie("token", {
+          httpOnly: true,
+          sameSite: "strict",
+        });
+        return res.status(200).json({
+            message:"Logout successfully"
+        })
+    } catch (error) {
+        console.log("Error: ",error)
+        return res.status(500).json({
+            message:"Internal Server Error"
+        });       
+    }
+}
+
+export {signup,login,logout};
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { signup,login } from '../controllers/user.controller.js';
+import { signup,login, logout } from '../controllers/user.controller.js';
 const userRouter = express.Router();
 userRouter.post('/signup',signup);
 userRouter.post('/login',login);
-
+userRouter.get('/logout',logout);
 
 export {userRouter}
